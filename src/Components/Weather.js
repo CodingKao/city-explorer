6013f6e9e281c9e63f65ff483138873400e953b4
Kao Saelor <saelor.kao@gmail.com>
2023-04-20 03:01:02 -0700

    edit

---
+++ src/Components/Weather.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+
+class CityWeather extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        const { forecasts } = this.props;
+        return (
+            <div>
+                <h3>7 Day Forecast</h3>
+                {forecasts.map((forecast, index) => (
+                    <Card key={index} style={{ marginBottom: '1rem' }}>
+                        <Card.Header>
+                            {forecast.date}
+                            <Button
+                                variant='link'
+                                onClick={() =>
+                                    this.setState((prevState) => ({
+                                        [index]: !prevState[index],
+                                    }))
+                                }
+                            >
+                                Show/Hide Details
+                            </Button>
+                        </Card.Header>
+                        {this.state[index] && (
+                            <Card.Body>
+                                <Card.Text>
+                                    Description: {forecast.description}
+                                </Card.Text>
+                                <Card.Text>
+                                    High Temperature: {forecast.maxTemp} °F
+                                </Card.Text>
+                                <Card.Text>
+                                    Low Temperature: {forecast.minTemp} °F
+                                </Card.Text>
+                            </Card.Body>
+                        )}
+                    </Card>
+                ))}
+            </div>
+        );
+    }
+}
+
+export default CityWeather;
