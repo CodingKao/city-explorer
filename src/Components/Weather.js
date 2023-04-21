ae045940933d20dbf8fabdc45cbda8e4b0e954df
Kao Saelor <saelor.kao@gmail.com>
2023-04-20 19:30:39 -0700

    remove unwanted comments

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,25 +1,14 @@
 import React from 'react';
-// import { Card, Button } from 'react-bootstrap';
 import WeatherDay from './WeatherDay';
 
 class Weather extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {};
-    // }
 
-    // eslint-disable-next-line react/require-render-return
     render() {
         console.log(this.props)
-        // let forecastElements;
-        // if (this.props.forecast) {
-        //     forecastElements = this.props.forecasts.map((forecast) => {
-
                 return (
                     <WeatherDay forecast={this.props.forecasts} />
                 )
                 };
-        // }
     }
 
 export default Weather;
